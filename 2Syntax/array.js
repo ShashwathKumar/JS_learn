a07818d58b27d709894c52e146ee0fb918de320e
Shashwath Kumar <shashwath.basics@gmail.com>
2016-04-01 12:57:58 -0400

    array working in html :), not yet in terminal

--- 2Syntax/array.js
+++ 2Syntax/array.js
@@ -1,12 +1,11 @@
 var phonebookEntry = {};
 
 console.log('in array function');
-document.write('In array func');
 
 phonebookEntry.name = 'Oxnard Montalvo';
 phonebookEntry.number = '(555) 555-5555';
 phonebookEntry.phone = function() {
-  console.log('Calling ' + this.name + ' at ' + this.number + '...');
-};
+    console.log('Calling ' + this.name + ' at ' + this.number + '...');
+}
 
 phonebookEntry.phone();
